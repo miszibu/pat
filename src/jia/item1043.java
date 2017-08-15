d9ea1e2c642f2ff62097b3af7f7da4f841e32b3e
zibu <806768001@qq.com>
2017-08-15 11:30:32 +0800

    8/15

---
+++ src/jia/item1043.java
@@ -0,0 +1,34 @@
+/*
+package jia;
+
+import java.util.Scanner;
+
+*/
+/**
+ * Created by zibu on 2017/8/7.
+ * The Dominant Color 内存超限
+ *//*
+
+public class item1043 {
+    public static void main(String [] args){
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        int max = 0;
+        int index=0;
+        int[] num = new int[33554432];
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++) {
+                num[sc.nextInt()]+=1;
+            }
+        }
+        for (int i=0;i<33554432;i++) {
+            if (num[i]>max){
+                max = num[i];
+                index = i;
+            }
+        }
+        System.out.println(index);
+    }
+}
+*/
